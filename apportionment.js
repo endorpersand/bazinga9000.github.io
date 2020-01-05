d3ca8a915127322c709fcb37fdc28eda39d94333
Bazinga <19626297+Bazinga9000@users.noreply.github.com>
2020-01-04 21:18:03 -0500

    first commit

---
+++ apportionment.js
@@ -0,0 +1,147 @@
+function clearStates() {
+    let metadiv = document.getElementById('state_inputs');
+    for (let elem of [...metadiv.children]) {
+        metadiv.removeChild(elem);
+    }
+}
+
+
+function applyTemplate(json) {
+    let metadiv = document.getElementById('state_inputs');
+
+    clearStates();
+
+    for (let elem of json.states) {
+        newState(elem.name, elem.population);
+    }
+
+    let seats = document.getElementById('seat_number');
+    seats.value = json.seats;
+}
+
+
+function newState(name = "New District", population = 2763) {
+    let metadiv = document.getElementById('state_inputs')
+
+// create the barebone elements for the row
+    let div = document.createElement('div'),     // div that holds the row
+        text1 = document.createTextNode('District:'),
+        box1 = document.createElement('input'),
+        text2 = document.createTextNode('Population:'),
+        box2 = document.createElement('input'),
+        kill = document.createElement('div'); // kill button
+
+// add element spice (give them attributes and css or whatever)
+    div.class = 'state';
+    box1.type = 'text';
+    box1.value = name
+    box2.type = 'number';
+    box2.value = population;
+    //input.classList.add('colors');
+    kill.innerHTML = 'x';
+    kill.classList.add('divbutton');
+
+// kill button
+    kill.addEventListener('click', e => {
+        e.preventDefault();    // i don't remember why i put this here
+        metadiv.removeChild(div); // kill the div
+    })
+
+// append all things to div and append div to parent
+    div.appendChild(text1);
+    div.appendChild(box1);
+    div.appendChild(text2);
+    div.appendChild(box2);
+    div.appendChild(kill);
+    metadiv.appendChild(div);
+}
+
+function apportion() {
+    let metadiv = document.getElementById('state_inputs')
+
+    //reduce old table to atoms
+    let oldtable = document.getElementById('apportionment_table');
+    if (oldtable !== null) {oldtable.outerHTML = "";}
+
+    //build district list
+    let districts = {};
+
+    for (let elem of [...metadiv.children]) {
+        let name = elem.children[0].value;
+        let population = elem.children[1].value;
+
+        districts[name] = [Number(population), 0, 0]; //population, seats, priority number
+    }
+
+    let seats_to_allocate = document.getElementById('seat_number').value;
+
+    let population_order = Object.keys(districts).sort((a, b) => districts[b][0] - districts[a][0])
+
+    seat_table = []
+
+    //start with 1 seat each, most population first
+    if (document.getElementById('start_with_one_each').value) {
+        for (let s of population_order) {
+            districts[s][1] += 1;
+            seats_to_allocate -= 1;
+            seat_table.push(s);
+            console.log("gave seat to " + s + ". " + seats_to_allocate + " seats left.")
+            if (seats_to_allocate == 0) {
+                break;
+            }
+        }
+    }
+
+    //use the huntington-hill method to apportion seats
+    while (seats_to_allocate > 0) {
+        //generate priority numbers
+        for (let s of population_order) {
+            districts[s][2] = districts[s][0] / Math.sqrt(districts[s][1] * (districts[s][1] + 1)); //p.n = population / sqrt(seats(seats+1))
+        }
+
+        //sort by priority number, highest gets the next seat
+        let next_seat = Object.keys(districts).sort((a, b) => districts[b][2] - districts[a][2])[0];
+        seat_table.push(next_seat);
+        districts[next_seat][1] += 1;
+        seats_to_allocate -= 1;
+        console.log("gave seat to " + next_seat + ". " + seats_to_allocate + " seats left.")
+
+
+    }
+
+    console.log(districts);
+
+    let tbl = document.createElement('table');
+    tbl.setAttribute('class','apportionment_table');
+    tbl.setAttribute('id','apportionment_table');
+
+    let head = document.createElement('thead');
+    let tr = document.createElement('tr');
+    for (s of ["District","Seats", "Population","Citizens per Seat"]) {
+        let th = document.createElement('th');
+        th.appendChild(document.createTextNode(s));
+        tr.appendChild(th);
+    }
+    head.appendChild(tr);
+    tbl.appendChild(head);
+
+    let body = document.createElement('tbody');
+
+    let color = 0
+    for (d of Object.keys(districts)) {
+        let r = document.createElement('tr');
+        if (color % 2 == 0) {r.setAttribute("class","light-color-row");}
+        color = (color + 1)%2
+        for (c of [d, districts[d][1], districts[d][0], Math.round(districts[d][0]/districts[d][1])]) {
+            let td = document.createElement('td');
+            td.appendChild(document.createTextNode(c));
+            r.appendChild(td);
+        }
+        body.appendChild(r);
+    }
+    tbl.appendChild(body);
+
+
+    document.body.appendChild(tbl);
+
+}
